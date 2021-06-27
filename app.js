d5d3b2fe5c55e0e3d467798c7562aa09b0edc3e7
EveNavarro <evenavarro.dn@gmail.com>
2021-06-27 01:03:03 -0300

    Agregué la URL '/turing'

--- app.js
+++ app.js
@@ -26,12 +26,16 @@ app.get('/clarke', (req, res) => {
 
 app.get('/hamilton', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/hamilton.html'))
-})
+});
 
 app.get('/hopper', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/hopper.html'))
-})
+});
 
 app.get('/lovelace', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/lovelace.html'))
-})
+});
+
+app.get('/turing', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/turing.html'))
+});
