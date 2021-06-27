0f6254f4f326578e33ae0e7a783d2521b71fbf91
EveNavarro <evenavarro.dn@gmail.com>
2021-06-27 00:19:01 -0300

    agregando las url, /home

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+let port = 3030; 
 const path = require('path');
 
 app.use(express.static('public'));
-app.listen(3030, () =>{
-    console.log('Servidor corriendo');
+
+app.listen(port, () =>
+    console.log(`Servidor levantado en el puerto ${port}\n http://localhost:${port}`));
+
+app.get('/home', (req, res)=>{
+    res.sendFile(path.join(__dirname, '/views/index.html'))
 });
